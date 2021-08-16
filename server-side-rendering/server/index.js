23b9b7f8da73a8aa26bd9ad0f03e563353f5382d
Sebastian <sfigueroa@pagalo.net.co>
2021-08-16 12:01:11 -0500

    Added streaming to server rendering

--- server-side-rendering/server/index.js
+++ server-side-rendering/server/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { renderToString } from 'react-dom/server';
+import { renderToNodeStream, renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 import fs from 'fs';
 import App from '../src/App';
@@ -13,6 +13,7 @@ const app = express();
 
 app.use('/dist',express.static('dist'));
 app.use((req,res) => {
+    res.write(parts[0]);
     const staticContext = {};
     const reacftMarkup = (
         <StaticRouter url={req.url} context={staticContext}>
@@ -20,9 +21,18 @@ app.use((req,res) => {
         </StaticRouter>
     );
 
+    const stream = renderToNodeStream(reacftMarkup);
+    stream.pipe(res,{end:false});
+    stream.on("end",() => {
+        res.status(staticContext || 200);
+        res.write(parts[1]);
+        res.end();
+    })
+    /*
     res.status(staticContext.statusCode || 200);
     res.send(`${parts[0]}${renderToString(reacftMarkup)}${parts[1]}`);
     res.end();
+    */
 });
 
 console.log(`Listening on http://localhost:${PORT}`);
