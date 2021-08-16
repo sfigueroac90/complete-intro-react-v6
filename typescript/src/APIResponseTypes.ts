b0db12af410ca641989fca3fffa440bca41b39a6
Sebastian <sfigueroa@pagalo.net.co>
2021-08-16 14:59:45 -0500

    Added typescript

---
+++ typescript/src/APIResponseTypes.ts
@@ -0,0 +1,27 @@
+export type Animal = "dog" | "cat" | "bird" | "reptile" | "rabbit";
+
+
+export interface Pet {
+    id: string;
+    name: string;
+    animal: Animal;
+    description: string;
+    breed: string;
+    images: string[];
+    city: string;
+    state: string;
+    color?: string;
+}
+
+export interface PetAPIResponse {
+    numberOfResults: number,
+    startIndex: number,
+    endIndex: number,
+    hasNext: boolean,
+    pets: Pet[]
+}
+
+export interface BreedListAPIResponse {
+    animal: Animal;
+    breeds: string[];
+}
